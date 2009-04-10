a60b528b600de9d247b4332a62754fe3d3e236dd
Christian Nelson <zero@zerolives.org>
2009-04-10 15:00:19 -0400

    version #

--- diggbarred.php
+++ diggbarred.php
@@ -3,13 +3,13 @@
 Plugin Name: Diggbarred
 Author: Phil Nelson
 Author URI: http://extrafuture.com/
-Version: 1.0
+Version: 1.01
 Description: Blocks all traffic from Digg from viewing your content, replacing it with a message to users. Idea and DiggBar code taken entirely from http://daringfireball.net/2009/04/how_to_block_the_diggbar
 Plugin URI: http://extrafuture.com/projects/diggbarred
 
 */
 
-add_option("diggbarred_version", "1.0");
+add_option("diggbarred_version", "1.01");
 add_option("diggbarred_message", "Dear Digg, Go fuck yourself.");
 
 add_action('init', 'diggbarred_do_the_shit');
