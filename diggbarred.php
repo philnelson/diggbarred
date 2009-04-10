eb1d54fd751746a38c20804c5d0321aab7e65b15
Christian Nelson <zero@zerolives.org>
2009-04-10 15:14:32 -0400

    New new regex pattern

--- diggbarred.php
+++ diggbarred.php
@@ -3,13 +3,13 @@
 Plugin Name: Diggbarred
 Author: Phil Nelson
 Author URI: http://extrafuture.com/
-Version: 1.01
+Version: 1.02
 Description: Blocks all traffic from Digg from viewing your content, replacing it with a message to users. Idea and DiggBar code taken entirely from http://daringfireball.net/2009/04/how_to_block_the_diggbar
 Plugin URI: http://extrafuture.com/projects/diggbarred
 
 */
 
-add_option("diggbarred_version", "1.01");
+add_option("diggbarred_version", "1.02");
 add_option("diggbarred_message", "Dear Digg, Go fuck yourself.");
 
 add_action('init', 'diggbarred_do_the_shit');
@@ -17,7 +17,7 @@ add_action('init', 'diggbarred_do_the_shit');
 function diggbarred_do_the_shit() 
 {
 	
-	if(preg_match('#http://digg.com/\w{1,8}[?/]*$#', $_SERVER['HTTP_REFERER'])) 
+	if(preg_match('#http://digg.com/\w{1,8}/*(\?.*)?$#', $_SERVER['HTTP_REFERER'])) 
 	{
 	    echo get_option('diggbarred_message');
 	    exit;
